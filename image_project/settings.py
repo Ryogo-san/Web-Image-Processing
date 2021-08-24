c6d5a9f4375179ae0902802ddce8809a87313321
Ryogo-san <ritosetsugekka0226@gmail.com>
2021-08-24 10:50:00 +0900

    initial commit

--- image_project/settings.py
+++ image_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY is in local_secret.py (not public)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
