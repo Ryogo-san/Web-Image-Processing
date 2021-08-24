8fd8b4e3bdb261f1ae97f9d63181f97ed2de565a
Ryogo-san <ritosetsugekka0226@gmail.com>
2021-08-24 11:41:21 +0900

    update README

--- image_project/settings.py
+++ image_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY is in local_secret.py (not public)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
